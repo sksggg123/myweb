8917a8ae8ebe551aabec375be779d2ced2faaa8d
권병윤 <sksggg123@gmail.com>
2019-03-05 22:34:16 +0900

    modify - controller init

--- src/main/java/com/sksggg123/myweb/web/MainController.java
+++ src/main/java/com/sksggg123/myweb/web/MainController.java
@@ -2,7 +2,6 @@ package com.sksggg123.myweb.web;
 
 import com.sksggg123.myweb.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
