64ca1f14e0e473edc764520873d2b16249ad5233
권병윤 <sksggg123@gmail.com>
2019-03-13 23:11:21 +0900

    step1_domain : rest api(postman)을 통한 h2-db 에 데이터 insert 하기

---
+++ src/main/java/com/sksggg123/myweb/dto/ContentsSaveRequestDto.java
@@ -0,0 +1,24 @@
+package com.sksggg123.myweb.dto;
+
+import com.sksggg123.myweb.domain.Contents;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ContentsSaveRequestDto {
+
+    private String link;
+    private String description;
+    private Long category;
+
+    public Contents toEntry() {
+        return Contents.builder()
+                .link(this.link)
+                .description(this.description)
+                .category(this.category)
+                .build();
+    }
+}
